87fd5c2b3af4cd33acb5e7bc1a4c8f6873aa2524
patriziopezzilli <patriziopezzilli@gmail.com>
2016-05-13 18:48:48 +0200

    Login-Registration Page + ListView

---
+++ src/BibliotecaDigitale/src/java/it/bibliotecadigitale/Servlet/List_Title.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bibliotecadigitale.Servlet;
+
+import it.bibliotecadigitale.Utility.FreeMarker;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Patrizio
+ */
+public class List_Title extends HttpServlet {
+ Map data= new HashMap();
+        
+    private void action_error(HttpServletRequest request, HttpServletResponse response) throws IOException {
+        //assumiamo che l'eccezione sia passata tramite gli attributi della request
+        //we assume that the exception has been passed using the request attributes
+        Exception exception = (Exception) request.getAttribute("exception");
+        String message;
+        if (exception != null && exception.getMessage() != null) {
+            message = exception.getMessage();
+        } else {
+            message = "Unknown error";
+        }
+        data.put("errore", message);
+        FreeMarker.process("404page.html", data, response, getServletContext());
+        
+      
+    }
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException{
+           
+        FreeMarker.process("list_title.html", data, response, getServletContext());
+}
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
---
+++ src/BibliotecaDigitale/src/java/it/bibliotecadigitale/Utility/FreeMarker.java
@@ -0,0 +1,72 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package it.bibliotecadigitale.Utility;
+
+
+import freemarker.ext.beans.BeansWrapper;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Map;
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletResponse;
+/**
+ *
+ * @author Patrizio
+ */
+public class FreeMarker {
+    /**
+     * 
+     * @param data              dati da inserire nel template          
+     * @param path_template     pathname del template da caricare
+     * @param response          
+     * @param servlet_context
+     * @throws IOException
+     */
+    public static void process(String path_template, Map data, HttpServletResponse response, ServletContext servlet_context) throws IOException{
+        
+        response.setContentType("text/html;charset=UTF-8");        
+        // Configurazione freemarker
+        Configuration cfg = new Configuration();
+        
+        cfg.setDefaultEncoding("UTF-8");
+            
+        cfg.setServletContextForTemplateLoading(servlet_context, "/OOP_Template");
+        
+        
+        
+        
+        BeansWrapper wrapper = (BeansWrapper) cfg.getObjectWrapper();
+        wrapper.setSimpleMapWrapper(true);
+        cfg.setObjectWrapper(wrapper);
+
+        
+        
+        
+        
+        Template template = cfg.getTemplate(path_template);
+        
+        PrintWriter out = response.getWriter();
+        
+        try{
+            template.process(data, out);
+            
+        } catch (TemplateException ex) {     
+            
+        } finally{
+            out.flush();
+            out.close(); 
+        }
+        
+        
+
+    }
+
+    
+    
+}
